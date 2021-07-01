51bfba2722c9c36dd5e379abc84d1d531925819b
chainvisions <chainvisions@outlook.com>
2021-06-30 18:34:52 -0600

    Remove strategies

--- contracts/strategies/IMasterchef.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.16;
-
-interface IMasterchef {
-    function enterStaking(uint256 _amount) external;
-    function leaveStaking(uint256 _amount) external;
-    function emergencyWithdraw(uint256 _pid) external;
-    function userInfo(uint256 _pid, address _user) external view returns(uint256, uint256);
-}
--- contracts/strategies/IVaiVault.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.16;
-
-interface IVaiVault {
-    function deposit(uint256 _amount) external;
-    function withdraw(uint256 _amount) external;
-    function claim() external;
-    function userInfo(address _user) external view returns(uint256, uint256);
-}
