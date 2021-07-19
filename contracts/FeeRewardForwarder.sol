c923683f2178ed5261a1b448e277c487d3c877b4
chainvisions <chainvisions@outlook.com>
2021-07-19 15:41:39 -0600

    More predefined routes

--- contracts/FeeRewardForwarder.sol
+++ contracts/FeeRewardForwarder.sol
@@ -14,15 +14,18 @@ contract FeeRewardForwarder is Governable {
   // Tokens for predefined routes.
   address public constant beluga = address(0x181dE8C57C4F25eBA9Fd27757BBd11Cc66a55d31);
   address public constant cake = address(0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82);
-  address public constant dot = address(0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402);
   address public constant wbnb = address(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
   address public constant busd = address(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
-  address public constant dodo = address(0x67ee3Cb086F8a16f34beE3ca72FAD36F7Db929e2);
-  address public constant nrv = address(0x42F6f551ae042cBe50C739158b4f0CAC0Edb9096);
-  address public constant xvs = address(0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63);
+  address public constant wings = address(0x0487b824c8261462F88940f97053E65bDb498446);
   address public constant fts = address(0x4437743ac02957068995c48E08465E0EE1769fBE);
+  address public constant stbb = address(0xE1316066af35fbfF54f870eA6d1468255602a696);
 
-  mapping (address => mapping (address => address[])) public routes;
+  // Routers for predefined routes.
+  address public constant pancakeRouter = address(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+  address public constant jetswapRouter = address(0xBe65b8f75B9F20f4C522e0067a3887FADa714800);
+
+  mapping(address => mapping (address => address[])) public routes;
+  mapping(address => mapping(address => address)) public targetRouter;
 
   // The targeted reward token to convert everything to
   address public targetToken;
@@ -37,10 +40,14 @@ contract FeeRewardForwarder is Governable {
     router = _router;
     // Predefined routes
     routes[cake][wbnb] = [cake, wbnb];
-    routes[dot][wbnb] = [dot, wbnb];
-    routes[dodo][wbnb] = [dodo, wbnb];
-    routes[nrv][wbnb] = [nrv, busd, wbnb];
-    routes[xvs][wbnb] = [xvs, wbnb];
+    routes[wings][wbnb] = [wings, wbnb];
+    routes[fts][wbnb] = [fts, wbnb];
+    routes[stbb][wbnb] = [stbb, wbnb];
+    // Predefined routers
+    targetRouter[cake][wbnb] = pancakeRouter;
+    targetRouter[wings][wbnb] = jetswapRouter;
+    targetRouter[fts][wbnb] = pancakeRouter;
+    targetRouter[stbb][wbnb] = pancakeRouter;
   }
 
   /*
